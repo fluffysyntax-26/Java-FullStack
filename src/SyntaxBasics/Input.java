1f0f8a0b903f30ed67a7c9ed53d49bdec658012b
Deepak <www.deepakkrishna9845@gmail.com>
2026-01-18 07:48:46 +0530

    Restructured core syntax into a separate folder

--- src/SyntaxBasics/Input.java
+++ src/SyntaxBasics/Input.java
@@ -1,3 +1,5 @@
+package SyntaxBasics;
+
 import java.util.Scanner;
 
 public class Input {
--- src/SyntaxBasics/variables.java
+++ src/SyntaxBasics/variables.java
@@ -1,3 +1,5 @@
+package SyntaxBasics;
+
 public class variables {
     public static void main(String[] args){
         // integer
